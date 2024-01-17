a62607dd2108ba3da0ed291786360ed62b1d4536
nopevbf <furimanjay@gmail.com>
2024-01-17 17:12:13 +0700

    initial commit

---
+++ config/config.php
@@ -0,0 +1,4 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<mikhmon','mikhmon>|>aWNlbA==');
+$data['NoorByte'] = array ('1'=>'NoorByte!192.168.77.1','NoorByte@|@noorbyte','NoorByte#|#n6OnqKWnqqCdamM=','NoorByte%NoorByte','NoorByte^noorbyte.co.id','NoorByte&Rp. ','NoorByte*085173370796','NoorByte(','NoorByte)','NoorByte=30','NoorByte@!@enable','NoorByte#!#');
